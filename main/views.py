8cf7fa9f9d4876b47ea2621bbfdbacf236bc7321
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-02-28 21:01:20 +0500

    start project

--- main/views.py
+++ main/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.http import HttpRequest,JsonResponse,FileResponse
+from rest_framework.parsers import MultiPartParser, FormParser
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
-# Create your views here.
+class UserView(APIView):
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'username': openapi.Schema(type=openapi.TYPE_STRING),
+            'password': openapi.Schema(type=openapi.TYPE_STRING),
+            'first_name': openapi.Schema(type=openapi.TYPE_STRING),
+        },
+        required=['username', 'password'],
+    ),
+    responses={
+            status.HTTP_201_CREATED: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'Status': openapi.Schema(type=openapi.TYPE_STRING),
+                    'Token': openapi.Schema(type=openapi.TYPE_STRING),
+                })
+            },
+    operation_description="Create a new user with the provided username, password, and optional first name.",
+    )
+    def post(self, request):
+        data = request.data
+        username = data.get('username',None)
+        password = data.get('password',None)
+        first_name = data.get('first_name',username)
+        if username is not None and first_name is not None:
+            try:
+                user = User.objects.get(username=username)
+                return Response({'Status':'This username is already'},status=status.HTTP_208_ALREADY_REPORTED)
+            except:
+                user = User.objects.create(
+                    username=username,
+                    password=make_password(password),
+                    first_name=first_name
+                )
+                user.save()
+                token = Token.objects.create(user=user)
+                return Response({'Status':'created','Token':token.key},status=status.HTTP_201_CREATED)
+        return Response({'Status':'BAD_REQUEST'},status=status.HTTP_400_BAD_REQUEST)
+class Signin(APIView):
+    authentication_classes = [BasicAuthentication]
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'username': openapi.Schema(type=openapi.TYPE_STRING),
+            'password': openapi.Schema(type=openapi.TYPE_STRING),
+        },
+        required=['username', 'password'],
+    ),
+    responses={
+            status.HTTP_200_OK: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'Status': openapi.Schema(type=openapi.TYPE_STRING),
+                    'Token': openapi.Schema(type=openapi.TYPE_STRING),
+                }),
+            status.HTTP_404_NOT_FOUND: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'Status': openapi.Schema(type=openapi.TYPE_STRING),
+                    'Token': openapi.Schema(type=openapi.TYPE_STRING, nullable=True),
+                })
+            },
+    security=[{"basic": []}],  # Specify Basic Authentication
+    operation_description="For login, request username and password, response status and user's token.",
+    )
+    
+    def post(self,request):
+        user = request.user
+        try:
+            token,created = Token.objects.get_or_create(user = user)
+            return Response({'Status': 'OK','Token': token.key},status=status.HTTP_200_OK)
+        except:
+            return Response({'Status': 'User not found', 'Token':None},status=status.HTTP_404_NOT_FOUND)
