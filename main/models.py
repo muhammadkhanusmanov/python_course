76b26ed8b797f88f2f1d827bb197f445b6d7fef3
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-02 10:50:37 +0500

    lesson model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Lesson(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=300)
+    file = models.FileField(upload_to='files/')
+
+    def __str__(self) -> str:
+        return self.name
