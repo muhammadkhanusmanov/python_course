9771d01360925be39db639ae64ac0e4dee00372e
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-04 18:31:42 +0500

    v3 url

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/',include('main.urls.v1_urls')),
     path('v2/',include('main.urls.v2_urls')),
+    path('v3/',include('main.urls.v3_urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
