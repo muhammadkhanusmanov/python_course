974f0fb17da0c8f6b12bfcda30d777a777427036
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-04 17:03:07 +0500

    url for get lesson

--- main/urls/v2_urls.py
+++ main/urls/v2_urls.py
@@ -5,5 +5,6 @@ from ..views import (CreatLesson,SaveFile,GetLessons)
 urlpatterns = [
     path('create_lesson/',CreatLesson.as_view()),
     path('save_file/<str:id>',SaveFile.as_view()),
-    path('get_lessons/', GetLessons.as_view())
+    path('get_lessons/', GetLessons.as_view()),
+    path('get_lesson/<str:id>', GetLessons.as_view())
 ]
