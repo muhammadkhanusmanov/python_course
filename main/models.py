16da57e9f7bf209008b49c1a079c8587b7455e12
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-04 18:30:51 +0500

    completed model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Lesson(models.Model):
     name = models.CharField(max_length=100)
@@ -7,3 +8,10 @@ class Lesson(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+class Completed(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lessons = models.ManyToManyField(Lesson)
+
+    def __str__(self):
+        return self.user.username
