848f5396860dbbe09ffa82da9c2ea066f553ba5c
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-04 18:00:03 +0500

    init

--- main/urls/v1_urls.py
+++ main/urls/v1_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from ..views import (Signup,Signin,Logout)
+from ..views import (Signup,Signin,Logout,GetUsers)
 
 urlpatterns = [
     path('signup/',Signup.as_view()),
     path('signin/',Signin.as_view()),
     path('logout/',Logout.as_view()),
+    path('getusers/',GetUsers.as_view()),
 ]
