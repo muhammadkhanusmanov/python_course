78e5aed3a9fa22bbd94b3cd77bac5511327a7078
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-05 18:15:05 +0500

    completed url

--- main/urls/v3_urls.py
+++ main/urls/v3_urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from ..views import (CompletedView)
 
 urlpatterns = [
+    path('getcompletes/',CompletedView.as_view()),
     path('addcompleted/', CompletedView.as_view()),
 ]
