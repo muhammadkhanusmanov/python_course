b66036436e3424493f5482744c6a245976859fba
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-03-05 18:20:42 +0500

    .

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'main',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
     'rest_framework_simplejwt',
     'django_extensions',
     'drf_yasg',
